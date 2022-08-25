e45b4d6b7094e12ba77add19d48e6b587fb85c74
Alexem <alexem.ufo@gmail.com>
2022-08-25 15:40:19 +0300

    added class validator

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,20 +1,36 @@
 import { UserRoles } from '../enum/userRolesEnum';
+import {
+  IsEmail,
+  IsDate,
+  IsString,
+  IsNotEmpty,
+} from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 const date: string = Date.now().toString();
 
 export class UserDto {
   @ApiPropertyOptional({ type: String })
+  @IsString()
   id?: string;
 
   @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsString()
   fullName?: string;
 
   @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsString()
   role?: UserRoles;
 
   @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
   email?: string;
 
   @ApiPropertyOptional({ type: String, format: 'date-time' })
+  @IsString()
+  @IsDate()
   createdAt?: string = date;
 }
