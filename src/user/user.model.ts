f81d5c0db3381fc49f70988329944572bf712e00
Alexem <alexem.ufo@gmail.com>
2022-09-01 22:18:09 +0300

    created model

---
+++ src/user/user.model.ts
@@ -0,0 +1,37 @@
+import { UserRoles } from './types/user-roles-enum';
+import {
+  IsEmail,
+  IsDate,
+  IsString,
+  IsNotEmpty,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+const date: string = Date.now().toString();
+
+export class User {
+
+  @ApiProperty({ example: 'Alex', description: 'fullname' })
+  @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsString()
+  fullName?: string;
+
+  @ApiProperty({ example: 'ADMIN', description: 'role' })
+  @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsString()
+  role?: UserRoles;
+
+  @ApiProperty({ example: 'test@gmail.com', description: 'email' })
+  @ApiPropertyOptional({ type: String })
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
+  email?: string;
+
+  @ApiProperty({ example: date, description: 'date' })
+  @ApiPropertyOptional({ type: String, format: 'date-time' })
+  @IsString()
+  @IsDate()
+  createdAt?: string = date;
+}
